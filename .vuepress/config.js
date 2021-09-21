b4b4d69d3a88ef1159929df5585d79c5b7f68289
comter1 <1069055297@qq.com>
2021-09-21 17:21:34 +0800

    .

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,43 @@
+const moment = require('moment');
+
+moment.locale("zh-cn");
+
 module.exports= {
     base:"/document/",
     title:"浪荡人儿",
     description:"简单笔记",
     head: [
       ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
+      ['link', { rel: 'manifest', href: '/manifest.json' }],
+      ['meta', { name: 'theme-color', content: '#c' }],
+      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+      ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+      ['link', { rel: 'apple-touch-icon', href: '/icons/t1-1.png' }],
+      ['link', { rel: 'mask-icon', href: '/icons/ts1.svg', color: '#c' }],
+      ['meta', { name: 'msapplication-TileImage', content: '/icons/t1-2.png' }],
+      ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
       ['meta', { name: 'author', content: '浪荡人儿' }],
       ['meta', { name: 'keywords', content: 'vuepress介绍，浪荡人儿，pwn记录' }]
+    ],
+    plugins: [
+      [
+        '@vuepress/last-updated',
+        {
+          transformer: (timestamp) => {
+            // 不要忘了安装 moment
+            return moment(timestamp).format("LLLL")
+          }
+        }
+      ],
+      [
+        '@vuepress/pwa', {
+                serviceWorker: true,
+                updatePopup: {
+                  message: "新的风暴已经出现",
+                  buttonText: "迎接新的风暴吧"
+                }
+              }
+      ]
     ],
     themeConfig : {
       lastUpdated: '最后更新',
