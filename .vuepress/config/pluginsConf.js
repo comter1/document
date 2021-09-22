b1fed4e37785b6f2954fe8348dcbc374346d2f75
comter1 <1069055297@qq.com>
2021-09-22 20:50:50 +0800

    .

--- .vuepress/config/pluginsConf.js
+++ .vuepress/config/pluginsConf.js
@@ -24,5 +24,8 @@ module.exports = {
         clientId: secret.clientId,
         clientSecret: secret.clientSecret,
         autoCreateIssue:true
-      }
+      },
+      '@vuepress/google-analytics':{
+        'ga': secret.ga
+      },
     }
